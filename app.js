4e83924158a5d4b6da694e463112e146291887a8
wyy <wangyangyang9998@gmail.com>
2025-06-06 14:41:46 +0300

    nmd

--- app.js
+++ app.js
@@ -4,13 +4,7 @@ const app = express()
 // get the port from env variable
 const PORT = process.env.PORT || 5000
 
-app.use(express.static('dist', {
-  setHeaders: (res, path) => {
-    if (path.endsWith('.html')) {
-      res.setHeader('Content-Type', 'text/html')
-    }
-  }
-}))
+app.use(express.static('dist'))
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`)
--- cypress/e2e/app.cy.js
+++
@@ -1,6 +0,0 @@
-describe('App', () => {
-  it('front page can be opened', () => {
-    cy.visit('http://localhost:5000', { failOnStatusCode: false })
-    cy.contains('Pokemon')
-  })
-})
