1150dfcb745d327f15974734ec04ff5541eaa8af
wyy <wangyangyang9998@gmail.com>
2025-06-06 14:35:33 +0300

    aaa

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ const app = express()
 // get the port from env variable
 const PORT = process.env.PORT || 5000
 
-app.use(express.static('dist'))
+app.use(express.static('dist', {
+  setHeaders: (res, path) => {
+    if (path.endsWith('.html')) {
+      res.setHeader('Content-Type', 'text/html')
+    }
+  }
+}))
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`)
